5fca7819f1c4fd43c73bb6d3dc4d707161c56ea0
DaramgeOvO <lovely21144@gmail.com>
2024-05-23 16:29:09 +0900

    commit

--- 20240523/배열.js
+++ 20240523/배열.js
@@ -39,3 +39,45 @@ colors.forEach((color,index,array)=>{
     console.log("두번째 매개변수", index);
     console.log("세번째 매개변수", array);
 });
+
+/* 객체의 배열 */
+const persons = [
+    {name: "Daramge", age: 33, gender : "female", job : "swan"},
+    {name: "Dotory", age: 33, gender : "male", job : "gammer"},
+    {name: "dahyae", age: 33, gender : "female", job : "nerse"},
+    {name: "sanghyuck", age: 33, gender : "male", job : "firefighter"}
+    
+]
+console.log(persons[0].name);
+console.log(persons[1].job);
+console.log(persons[3].age);
+
+
+/* JavaScript의 구조분해 할당(destructuring assignment)은 
+배열또는 객체의 값을 쉽게 추출하여 변수에 할당할 수 있도록 하는 문법입니다. 
+이는 코드의 가독성을 높이고, 더 간결하게 작성할 수 있게 도와줍니다.*/
+const [브니, 재원, 다혜, 상혁] = persons;
+console.log(브니);
+const{name, age, gender, job} = 재원;
+console.log(name, age, gender, job);
+
+
+const nums = [1,2,3,4,5,6,7,8,9];
+/* filter
+조건에 맞는 데이터만 새로운 배열에 담는다.
+특징 : 데이터의 그대로, 새로운 배열의 length는 달라질 수 있음 */
+const array1 = nums.filter((num)=> num>5);
+console.log(array1);
+
+
+/*map
+데이터를 조작하여 새로운 배열에 담는다
+특징 : 데이터는 변경, 새로운 배열의 length는 반드시 동일 */
+const array2 = nums.map((num)=> num*10);
+console.log(array2);
+
+const jobArray = persons.map((p)=>p.job);
+console.log(jobArray);
+
+const femaleJobs = persons.filter((p)=>p.gender =="female").map((p)=> p.job);
+console.log(femaleJobs);
